2fc068208aabda090324d65207a4dcf511c8d97c
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-06 20:16:02 -0300

    test: ensure return 400 if no name is provided

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,19 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('should return 400 if no name is provided', () => {
+    const makeSut = new SignUpController()
+    const httpRequest = ({
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+        userAdmin: false,
+        createdDate: new Date(),
+        updatedDate: new Date()
+      }
+    })
+    const httpResponse = makeSut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
