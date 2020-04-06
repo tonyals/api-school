201521c23a5e6ce13bb5ead464aaf997a5ad0fca
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-06 18:57:23 -0300

    chore: add dependences

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
