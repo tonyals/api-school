0ae0dfb928dcd66de9e9607817904abbd2e1f98d
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-07 12:12:41 -0300

    refactor: add protocol HttpResponse and HttpRequest

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
