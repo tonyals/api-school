a5f5e836ff7784d3aa5a3f834e4e40fe68c6306d
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-07 11:31:08 -0300

    test: add MissingParamError to customize errors

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', () => {
@@ -8,14 +9,12 @@ describe('SignUp Controller', () => {
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password',
-        userAdmin: false,
-        createdDate: new Date(),
-        updatedDate: new Date()
+        userAdmin: false
       }
     })
     const httpResponse = makeSut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('should return 400 if no email is provided', () => {
@@ -25,13 +24,11 @@ describe('SignUp Controller', () => {
         name: 'any_name',
         password: 'any_password',
         passwordConfirmation: 'any_password',
-        userAdmin: false,
-        createdDate: new Date(),
-        updatedDate: new Date()
+        userAdmin: false
       }
     })
     const httpResponse = makeSut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 })
