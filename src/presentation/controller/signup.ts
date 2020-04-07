65ab0d67fa4514759d5b156f2339267ecc965164
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-07 11:55:02 -0300

    refactor: simplify code

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -2,24 +2,13 @@ import { MissingParamError } from '../errors/missing-param-error'
 
 export class SignUpController {
   handle (httpRequest: any): any {
-    if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
-    }
-
-    if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email')
-      }
-    }
-
-    if (!httpRequest.body.password) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('password')
+    const requiredFields = ['name', 'email', 'password']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return {
+          statusCode: 400,
+          body: new MissingParamError(field)
+        }
       }
     }
   }
