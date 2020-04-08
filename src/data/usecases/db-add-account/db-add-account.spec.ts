3ac96de9179848ad65e1588bc3f8763fe7fde134
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-08 10:53:11 -0300

    test: ensure DbAddAccount return an account on success

--- src/data/usecases/db-add-account/db-add-account.spec.ts
+++ src/data/usecases/db-add-account/db-add-account.spec.ts
@@ -97,4 +97,20 @@ describe('DbAddAccount Usecase', () => {
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
   })
+
+  test('should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 0,
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
