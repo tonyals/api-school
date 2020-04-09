9ba2dd7c8883ad8845480b931bff1101d070e366
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-09 15:36:55 -0300

    refactor: connect to Postgres before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,12 @@
 import request from 'supertest'
 import app from '../config/app'
+import { CreateTypeOrmConn } from '../../infra/db/postgres/create-typeorm-connection'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await CreateTypeOrmConn.connect()
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
