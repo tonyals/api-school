71753adbdc30c8c1751b0e5a48d2cc9ff6b54dc2
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-13 15:46:42 -0300

    feat: ensure LogMongoRepository creates an error log on success

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,25 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (url: string): Promise<void> {
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectiontWithoutId } = collection
+    return Object.assign({}, collectiontWithoutId, { id: _id })
+  }
+}
