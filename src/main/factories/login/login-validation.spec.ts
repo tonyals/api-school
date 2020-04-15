958d61ff59483bc75dea1c796265e5914f465381
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-15 15:28:01 -0300

    refactor: group validations imports in a single file

--- src/main/factories/login/login-validation.spec.ts
+++ src/main/factories/login/login-validation.spec.ts
@@ -1,9 +1,11 @@
+import {
+  ValidationComposite,
+  RequiredFieldValidation,
+  EmailValidation
+} from '../../../presentation/helpers/validators'
 import { makeLoginValidation } from './login-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../../presentation/protocols/validation'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 const makeEmailValidator = (): EmailValidator => {
--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,12 @@
+import {
+  ValidationComposite,
+  RequiredFieldValidation,
+  EmailValidation,
+  CompareFieldsValidation
+} from '../../../presentation/helpers/validators'
 import { makeSignUpValidation } from './signup-validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
 import { Validation } from '../../../presentation/protocols/validation'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 const makeEmailValidator = (): EmailValidator => {
