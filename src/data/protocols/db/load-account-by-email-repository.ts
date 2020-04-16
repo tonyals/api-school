4597f6d33b67088739a8e43cbc6b08220b5ccecd
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-16 15:08:37 -0300

    refactor: rename load to loadByEmail

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../../model/account'
 
 export interface LoadAccountByEmailRepository {
-  load (email: string): Promise<AccountModel>
+  loadByEmail (email: string): Promise<AccountModel>
 }
