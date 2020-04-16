e4a64a873a9127b1294a98d2b4c1e15c4761c255
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-16 16:38:48 -0300

    feat: ensure AccountPostgresRepository returns an account on loadByEmail success

--- src/infra/db/postgres/account-repository/account-save.ts
+++ src/infra/db/postgres/account-repository/account-save.ts
@@ -2,10 +2,17 @@ import { AddAccountRepository } from '../../../../data/protocols/db/add-account-
 import { AddAccountModel } from '../../../../data/model/add-account'
 import { AccountModel } from '../../../../data/model/account'
 import { User } from '../../../../entity/User'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 
-export class AccountPostgresRepository implements AddAccountRepository {
+export class AccountPostgresRepository implements AddAccountRepository,
+LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const account = await User.create(accountData).save()
+    return account
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const account = await User.findOne({ email })
     return account
   }
 }
