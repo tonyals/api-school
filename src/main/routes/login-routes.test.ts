6063fdc2cf248cbfcc1497a6121a1dbfc79ef68b
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-17 14:27:05 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,14 +3,18 @@ import { getConnection } from 'typeorm'
 import { CreateConnectionPostgres } from '../../infra/db/postgres/create-connection-postgres'
 import app from '../config/app'
 import { User } from '../../entity/User'
+import { hash } from 'bcrypt'
 
 describe('Login Routes', () => {
   beforeAll(async () => {
     await CreateConnectionPostgres.connect()
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     await User.delete({})
+  })
+
+  afterAll(() => {
     return getConnection(process.env.NODE_ENV).close
   })
 
@@ -19,11 +23,31 @@ describe('Login Routes', () => {
       await request(app)
         .post('/api/signup')
         .send({
-          name: 'Tony',
+          name: 'Tony Augusto',
           email: 'valid_email@mail.com',
           password: 'valid_password',
           passwordConfirmation: 'valid_password',
           isAdmin: true || false
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('1234', 12)
+      const user = {
+        name: 'Tony Augusto',
+        email: 'valid_email@mail.com',
+        password,
+        isAdmin: false
+      }
+      await User.create(user).save()
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.com',
+          password: '1234'
         })
         .expect(200)
     })
