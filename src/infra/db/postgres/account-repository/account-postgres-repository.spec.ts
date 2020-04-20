5f8aa7f4fb38f2e6675461c29ceb6e5249c92770
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-20 17:39:59 -0300

    test: ensure AccountPostgresRepository find an account by token without role

--- src/infra/db/postgres/account-repository/account-postgres-repository.spec.ts
+++ src/infra/db/postgres/account-repository/account-postgres-repository.spec.ts
@@ -75,4 +75,24 @@ describe('Account Postgres Repository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
+
+  describe('LoadByToken', () => {
+    test('should return an account on loadByToken without role', async () => {
+      const sut = new AccountPostgresRepository()
+      await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        isAdmin: true,
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.isAdmin).toBe(false || true)
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
 })
