ab19f6fdf2bdce56e6033aa285ac97b5780445f8
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-21 11:37:12 -0300

    refactor: change AddSurvey to insert a date also

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -33,7 +33,8 @@ describe('Survey Mongo Repository', () => {
       {
         answer: 'other_answer'
       }
-      ]
+      ],
+      date: new Date()
     })
     const survey = await surveyCollection.findOne({ question: 'any_question' })
     expect(survey).toBeTruthy()
--- src/presentation/controller/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controller/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, Validation, AddSurveyModel, AddSurvey } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 import { badRequest, serverError, noContent } from '../../../helpers/http/http-helper'
+import MockDate from 'mockdate'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -8,7 +9,8 @@ const makeFakeRequest = (): HttpRequest => ({
     answers: [{
       image: 'any_image',
       answer: 'any_answer'
-    }]
+    }],
+    date: new Date()
   }
 })
 
@@ -46,6 +48,13 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AddSurvey controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('should call validation with correct values', async () => {
     const { validationStub, sut } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
