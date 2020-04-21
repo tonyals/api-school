2b410b7b94f4d4a09878f9741252a8429523ac46
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-21 14:30:17 -0300

    test: ensure load surveys routes returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -75,4 +75,12 @@ describe('Survey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    test('should return 403 on load surveys without access token', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+  })
 })
