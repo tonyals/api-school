bfa9f657c6a837ccee485951cb4ae657c501985c
Tony Augusto Luz <tony_luzsilva@hotmail.com>
2020-04-21 12:13:10 -0300

    feat: ensure LoadSurveysController calls LoadSurveyUseCase

---
+++ src/presentation/controller/survey/load-surveys/load-survey-controller-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols/http'
+export * from '../../../protocols/controller'
+export * from '../../../../domain/usecases/load-surveys'
+export * from '../../../../domain/models/survey'
